09d424d67ca56bcd5bb4c40682a71ddcf274a7a3
Erwann Millon <erwann@Erwanns-MacBook-Air.local>
2023-01-16 12:40:01 -0500

    change default path

--- loaders.py
+++ loaders.py
@@ -17,10 +17,10 @@ def load_config(config_path, display=False):
 
 
 def load_default(device):
-    conf_path = "./celeba_vqgan/vqgan_only.yaml"
+    conf_path = "./model_checkpoints/vqgan_only.yaml"
     config = load_config(conf_path, display=False)
     model = taming.models.vqgan.VQModel(**config.model.params)
-    sd = torch.load("./celeba_vqgan/vqgan_only.pt", map_location=device)
+    sd = torch.load("./model_checkpoints/vqgan_only.pt", map_location=device)
     model.load_state_dict(sd, strict=True)
     model.to(device)
     del sd
